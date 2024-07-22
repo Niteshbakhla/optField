87dc8ac584ee677a48428035432f040d6aa50ad5
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-07-22 23:27:30 +0530

    added a paste functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [otpFields, setOtpFields] = useState(new Array(4).fill(""))
+  const ref = useRef([])
+
+  useEffect(() => {
+    ref.current[0].focus()
+  }, []);
+
+  const backHandleChange = (e, index) => {
+    const copyOtpFields = [...otpFields]
+    if (e.keyCode === 8) {
+      if (copyOtpFields[index] === "" && index > 0) {
+        ref.current[index - 1].focus();
+      } else {
+        copyOtpFields[index] = ""
+      }
+      setOtpFields(copyOtpFields);
+    }
+  }
+
+  const handlePaste = (e) => {
+    const data = e.clipboardData.getData("text")
+
+    if (!Number(data) || data.length !== otpFields.length) {
+      return
+    }
+
+    const pastCode = data.split("");
+    setOtpFields(pastCode);
+    ref.current[otpFields.length - 1].focus();
+  }
+
+  const handleChange = (e, index) => {
+    const val = e.target.value
+    const copyOtpFields = [...otpFields]
+    copyOtpFields[index] = val
+    setOtpFields(copyOtpFields); // Update state first
+    if (index < copyOtpFields.length - 1 && val) {
+      ref.current[index + 1].focus()
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='parent'>
+        <h1>Enter otp</h1>
+        <div>
+          {
+            otpFields.map((data, index) => (
+              <input
+                value={data}
+                ref={(currentInput) => ref.current[index] = currentInput}
+                key={index}
+                maxLength={1}
+                type='text'
+                onPaste={handlePaste}
+                onChange={(e) => handleChange(e, index)}
+                onKeyDown={(e) => backHandleChange(e, index)}
+              />
+            ))
+          }
+
+        </div>
+        <button>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
